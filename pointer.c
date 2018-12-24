fb034359c78f7125cb30085112f52e00cb316fc1
EnzioKam <enziokamhh@gmail.com>
2018-12-24 14:45:04 +0800

    Third commit

---
+++ pointer.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+
+    int var = 20;
+    int *ip = NULL;
+    printf("Address stored in ip variable: %x\n", ip);
+    ip = &var;
+
+    printf("Address of var variable: %x\n", &var);
+    printf("Address stored in ip variable: %x\n", ip);
+    printf("Value of *ip variable: %d\n", *ip);
+
+    return 0;
+}
---
+++ putgetio.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main( ) {
+
+	/*
+    int c;
+    printf("Enter a value:");
+    c = getchar( );
+    printf("\nYou entered: ");
+    putchar(c);
+    */
+
+  	char str[100];
+  	printf( "Enter a value :");
+  	gets(str);
+  	printf( "\nYou entered: ");
+  	puts(str);
+
+    return 0;
+}
